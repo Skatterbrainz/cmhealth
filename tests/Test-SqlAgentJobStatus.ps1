0a9ff183270972225435335537ec8112a21be51e
skatterbrainz <dstein2017@gmail.com>
2020-03-10 02:30:19 -0400

    new file

---
+++ tests/Test-SqlAgentJobStatus.ps1
@@ -0,0 +1,20 @@
+[CmdletBinding()]
+param (
+	[parameter(Mandatory)][ValidateNotNullOrEmpty()][hashtable] $ScriptParams
+)
+Write-Verbose "test: sql agent job status"
+$HoursBack = 24
+try {
+	$params = @{
+		SqlInstance = $ScriptParams.SqlInstance 
+		StartDate   = (Get-Date).AddHours(-$HoursBack)
+	}
+	$res = (Get-DbaAgentJobHistory @params | Where-Object {$_.Status -ne "Succeeded"})
+	if ($res.Count -gt 0) { $result = 'FAIL' } else { $result = 'PASS' }
+}
+catch {
+	$result = 'ERROR'
+}
+finally {
+	$result
+}
