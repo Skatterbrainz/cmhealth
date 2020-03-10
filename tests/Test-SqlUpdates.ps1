b5c2c1f4aae85b959976c23e65b5b81e5bee1900
skatterbrainz <dstein2017@gmail.com>
2020-03-10 02:20:09 -0400

    new file

---
+++ tests/Test-SqlUpdates.ps1
@@ -0,0 +1,15 @@
+[CmdletBinding()]
+param (
+	[parameter(Mandatory)][ValidateNotNullOrEmpty()][hashtable] $ScriptParams
+)
+Write-Verbose "test: sql instance updates"
+try {
+	$res = Test-DbaBuild -Latest -SqlInstance $ScriptParams.SqlInstance
+	if ($res.Compliant -eq $True) { $result = 'PASS' } else { $result = 'FAIL' }
+}
+catch {
+	$result = 'ERROR'
+}
+finally {
+	$result 
+}
