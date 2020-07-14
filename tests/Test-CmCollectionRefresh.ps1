6502e9b7a494f64e8cb7ddf2f2b82af097155e68
skatterbrainz <dstein2017@gmail.com>
2020-07-14 01:51:25 -0400

    new tests and support files

---
+++ tests/Test-CmCollectionRefresh.ps1
@@ -0,0 +1,50 @@
+function Test-CmCollectionRefresh {
+	[CmdletBinding()]
+	param (
+		[parameter()][string] $TestName = "Descriptive Name",
+		[parameter()][string] $TestGroup = "configuration",
+		[parameter()][string] $Description = "Description of this test",
+		[parameter()][bool] $Remediate = $False,
+		[parameter()][string] $SqlInstance = "localhost",
+		[parameter()][string] $Database = ""
+	)
+	try {
+		[System.Collections.Generic.List[PSObject]]$tempdata = @() # for detailed test output to return if needed
+		$stat = "PASS"
+		<# 
+		DELETE THIS COMMENT BLOCK WHEN FINISHED:
+		perform test and return result as an object...
+			$stat = 'PASS' or 'FAIL'
+			$msg = "whatever you want to provide"
+		#>
+		$query = "Select (Case when RefreshType = 1 then 'Manual'
+when RefreshType = 2 then 'Scheduled'
+when RefreshType = 4 then 'Incremental'
+when RefreshType = 6 then 'Scheduled and Incremental'
+else 'Unknown' end) as RefreshType, count(SiteID) as Collections
+from v_Collections
+group by RefreshType"
+		$cdat = Invoke-DbaQuery -SqlInstance $SqlInstance -Database $Database -Query $query
+		$c1 = $cdat | Where-Object {$_.RefreshType -eq 'Incremental'}
+		$c2 = $cdat | Where-Object {$_.RefreshType -eq 'Scheduled'}
+		$c3 = $c1 + $c2
+		if ($c3 -gt 200) {
+			$stat = "FAIL"
+			$msg  = "More than 200 collections are incremental or scheduled"
+		}
+	}
+	catch {
+		$stat = 'ERROR'
+		$msg = $_.Exception.Message -join ';'
+	}
+	finally {
+		Write-Output $([pscustomobject]@{
+			TestName    = $TestName
+			TestGroup   = $TestGroup
+			TestData    = $tempdata
+			Description = $Description
+			Status      = $stat 
+			Message     = $msg
+		})
+	}
+}
