dadeb0c8ab18f6ace448e032297673e404543cd1
Skatterbrainz <dstein2017@gmail.com>
2020-07-18 13:38:19 -0400

    new test

---
+++ tests/Test-CmDpNoGroups.ps1
@@ -0,0 +1,36 @@
+function Test-CmDpNoGroups {
+	[CmdletBinding()]
+	param (
+		[parameter()][string] $TestName = "Test-CmDpNoGroups",
+		[parameter()][string] $TestGroup = "configuration",
+		[parameter()][string] $Description = "Check for DPs which are not in a DP group",
+		[parameter()][hashtable] $ScriptParams
+	)
+	try {
+		[System.Collections.Generic.List[PSObject]]$tempdata = @() # for detailed test output to return if needed
+		$stat = "PASS" # do not change this
+		$msg  = "No issues found" # do not change this either
+		# FOR SQL QUERY RELATED TESTS... DELETE THIS BLOCK IF NOT USED
+		$query = "SELECT ServerName FROM v_DistributionPointInfo WHERE GroupCount < 1"
+		$res = @(Invoke-DbaQuery -SqlInstance $ScriptParams.SqlInstance -Database $ScriptParams.Database -Query $query)
+		if ($null -ne $res -and $res.Count -gt 0) {
+			$stat = "WARNING" # or "FAIL"
+			$msg  = "$($res.Count) items found: $($res.ServerName -join ',')"
+			$res | Foreach-Object {$tempdata.Add($_.ServerName)}
+		}
+	}
+	catch {
+		$stat = 'ERROR'
+		$msg = $_.Exception.Message -join ';'
+	}
+	finally {
+		Write-Output $([pscustomobject]@{
+			TestName    = $TestName
+			TestGroup   = $TestGroup
+			TestData    = $tempdata
+			Description = $Description
+			Status      = $stat 
+			Message     = $msg
+		})
+	}
+}
---
+++ tests/Test-CmInvMaxMIFSize.ps1
@@ -0,0 +1,40 @@
+function Test-CmInvMaxMIFSize {
+	[CmdletBinding()]
+	param (
+		[parameter()][string] $TestName = "Test-CmInvMaxMIFSize",
+		[parameter()][string] $TestGroup = "configuration",
+		[parameter()][string] $Description = "Validate inventory loader maximum MIF file size setting",
+		[parameter()][hashtable] $ScriptParams
+	)
+	try {
+		[System.Collections.Generic.List[PSObject]]$tempdata = @() # for detailed test output to return if needed
+		$stat = "PASS" # do not change this
+		$msg  = "No issues found" # do not change this either
+		$MaxMIF = 52428800
+		$res = (Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\SMS\COMPONENTS\SMS_INVENTORY_DATA_LOADER" -Name "Max MIF Size" | Select-Object -ExpandProperty "Max MIF Size")
+		if ($res -lt $MaxMIF) {
+			if ($ScriptParams.Remediate -eq $True) {
+				Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\SMS\COMPONENTS\SMS_INVENTORY_DATA_LOADER" -Name 'Max MIF Size' -Value $MaxMIF -Force | Out-Null
+				$stat = "REMEDIATED"
+				$msg  = "Max MIF Size is now set to $MaxMIF"
+			} else {
+				$stat = "FAIL"
+				$msg  = "Max MIF size is $res (hex) which should be 3200000 (hex) or $MaxMIF"
+			}
+		}
+	}
+	catch {
+		$stat = 'ERROR'
+		$msg = $_.Exception.Message -join ';'
+	}
+	finally {
+		Write-Output $([pscustomobject]@{
+			TestName    = $TestName
+			TestGroup   = $TestGroup
+			TestData    = $tempdata
+			Description = $Description
+			Status      = $stat 
+			Message     = $msg
+		})
+	}
+}
