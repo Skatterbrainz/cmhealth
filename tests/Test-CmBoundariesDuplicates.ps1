29a1e30887dcfefdf0bae8401347d65e7e547004
skatterbrainz <dstein2017@gmail.com>
2020-08-05 12:55:17 -0400

    split into separate tests

--- tests/Test-CmBoundariesDuplicates.ps1
+++ tests/Test-CmBoundariesDuplicates.ps1
@@ -1,15 +1,15 @@
 function Test-CmBoundaries {
 	[CmdletBinding()]
 	param (
-		[parameter()][string] $TestName = "Test-CmBoundaries",
+		[parameter()][string] $TestName = "Test-CmBoundariesDuplicates",
 		[parameter()][string] $TestGroup = "configuration",
-		[parameter()][string] $Description = "Validate Site Boundaries",
+		[parameter()][string] $Description = "Validate Site Boundaries are not duplicated",
 		[parameter()][hashtable] $ScriptParams
 	)
 	try {
 		[System.Collections.Generic.List[PSObject]]$tempdata = @() # for detailed test output to return if needed
-		$stat = "PASS"
-		$msg = "No issues found"
+		$stat  = "PASS"
+		$msg   = "No issues found"
 		$query = "select * from vSMS_Boundary"
 		if ($ScriptParams.Credential) {
 			$boundaries = @(Invoke-DbaQuery -SqlInstance $ScriptParams.SqlInstance -Database $ScriptParams.Database -Query $query -SqlCredential $ScriptParams.Credential)
@@ -17,20 +17,8 @@ function Test-CmBoundaries {
 			$boundaries = @(Invoke-DbaQuery -SqlInstance $ScriptParams.SqlInstance -Database $ScriptParams.Database -Query $query)
 		}
 		
-		$dupes = $boundaries | Group-Object -Property BoundaryType,Value | Select-Object Count,Name
-		$orphaned = $boundaries | Where-Object {$_.GroupCount -eq 0}
-		switch ($ScriptParams.Test) {
-			'DuplicateBoundaries' {
-				if (($dupes | Where-Object {$_.Count -gt 1}) -gt 0) {
-					$stat = 'FAIL'
-				}
-			}
-			'Orphaned' {
-				if ($orphaned.Count -gt 1) {
-					$stat = 'FAIL'
-				}
-			}
-		}
+		$dupes = @($boundaries | Group-Object -Property BoundaryType,Value | Select-Object Count,Name)
+		if (($dupes | Where-Object {$_.Count -gt 1}) -gt 0) { $stat = 'FAIL' }
 	}
 	catch {
 		$stat = 'ERROR'
