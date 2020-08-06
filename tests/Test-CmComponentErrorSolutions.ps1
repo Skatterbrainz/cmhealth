7c82e1a57f2bf6409a89e07f9146c66e9217d1eb
Skatterbrainz <dstein2017@gmail.com>
2020-08-06 01:26:49 -0400

    added config source

--- tests/Test-CmComponentErrorSolutions.ps1
+++ tests/Test-CmComponentErrorSolutions.ps1
@@ -7,6 +7,7 @@ function Test-CmComponentErrorSolutions {
 		[parameter()][hashtable] $ScriptParams
 	)
 	try {
+		[int] $DaysBack = Get-CmHealthDefaultValue -KeySet "configmgr:ComponentErrorsMaxDaysOld" -DataSet $CmHealthConfig
 		[System.Collections.Generic.List[PSObject]]$tempdata = @() # for detailed test output to return if needed
 		$stat = "PASS" # do not change this
 		$msg  = "No issues found" # do not change this either
@@ -17,7 +18,7 @@ stat.Severity IN (-1073741824, -2147483648)
 AND stat.Component NOT IN ('Advanced Client', 'Windows Installer SourceList Update Agent', 
 'Desired Configuration Management', 'Software Updates Scan Agent', 'File Collection Agent', 
 'Hardware Inventory Agent', 'Software Distribution', 'Software Inventory Agent')
-AND stat.Time >= DATEADD(dd,-CONVERT(INT,$($ScriptParams.DaysBack)),GETDATE())"
+AND stat.Time >= DATEADD(dd,-CONVERT(INT,$($DaysBack)),GETDATE())"
 		if ($ScriptParams.Credential) {
 			$res = @(Invoke-DbaQuery -SqlInstance $ScriptParams.SqlInstance -Database $ScriptParams.Database -Query $query -SqlCredential $ScriptParams.Credential)
 		} else {
@@ -25,8 +26,8 @@ AND stat.Time >= DATEADD(dd,-CONVERT(INT,$($ScriptParams.DaysBack)),GETDATE())"
 		}
 		if ($null -ne $res -and $res.Count -gt 0) {
 			$stat = "WARNING" # or "FAIL"
-			$msg  = "$($res.Count) items found: $($res.Name -join ',')"
-			#$res | Foreach-Object {$tempdata.Add($_.Name)}
+			$msg  = "$($res.Count) items found within the last $DaysBack days"
+			$res | Foreach-Object {$tempdata.Add($_.Component)}
 		}
 	}
 	catch {
