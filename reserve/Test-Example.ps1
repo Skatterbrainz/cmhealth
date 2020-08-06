068f53dd90a3f6a43060a865278e773709d7c2f5
Skatterbrainz <dstein2017@gmail.com>
2020-08-06 00:36:24 -0400

    updated template with WMI/CIM code

--- reserve/Test-Example.ps1
+++ reserve/Test-Example.ps1
@@ -12,15 +12,22 @@ function Test-Example {
 		$stat = "PASS" # do not change this
 		$msg  = "No issues found" # do not change this either
 		<# 
-		DELETE THIS COMMENT BLOCK WHEN FINISHED:
-		perform test and return result as an object...
-			$stat = "FAIL" or "WARNING" (no need to set "PASS" since it's the default)
-			$msg = (details of failure or warning)
-			loop output into $tempdata.Add() array to return as TestData param in output
+		=======================================================
+		|	COMMENT: DELETE THIS BLOCK WHEN FINISHED:
+		|
+		|	perform test and return result as an object...
+		|		$stat = "FAIL" or "WARNING" (no need to set "PASS" since it's the default)
+		|		$msg = (details of failure or warning)
+		|		add supporting data to $tempdata array if it helps output
+		|		loop output into $tempdata.Add() array to return as TestData param in output
+		=======================================================
 		#>
 
 		<#
-		# FOR SQL QUERY RELATED TESTS... DELETE THIS BLOCK IF NOT USED
+		=======================================================
+		COMMENT: EXAMPLE FOR SQL QUERY RELATED TESTS... DELETE THIS BLOCK IF NOT USED
+		=======================================================
+
 		$query = ""
 		if ($ScriptParams.Credential) {
 			$res = @(Invoke-DbaQuery -SqlInstance $ScriptParams.SqlInstance -Database $ScriptParams.Database -Query $query -SqlCredential $ScriptParams.Credential)
@@ -28,11 +35,35 @@ function Test-Example {
 			$res = @(Invoke-DbaQuery -SqlInstance $ScriptParams.SqlInstance -Database $ScriptParams.Database -Query $query)
 		}
 		
-		if ($null -ne $res -and $res.Count -gt 0) {
+		if ($res.Count -gt 0) {
 			$stat = "WARNING" # or "FAIL"
-			$msg  = "$($res.Count) items found: $($res.Name -join ',')"
+			$msg  = "$($res.Count) items found"
 			#$res | Foreach-Object {$tempdata.Add($_.Name)}
+		} 
+		#>
+
+		<#
+		=======================================================
+		COMMENT: EXAMPLE FOR WMI/CIM QUERY RELATED TESTS... DELETE THIS BLOCK IF NOT USED
+		=======================================================
+
+		if ($ScriptParams.ComputerName -ne $env:COMPUTERNAME) {
+			if ($ScriptParams.Credential) {
+				$cs = New-CimSession -Credential $ScriptParams.Credential -Authentication Negotiate -ComputerName $ScriptParams.ComputerName
+				$services = @(Get-CimInstance -CimSession $cs -ClassName Win32_Service | Where-Object {$_.StartMode -match 'auto' -and $_.State -ne 'Running'})
+			} else {
+				$services = @(Get-CimInstance -ComputerName $ScriptParams.ComputerName -ClassName Win32_Service | Where-Object {$_.StartMode -match 'auto' -and $_.State -ne 'Running'})
+			}
+		} else {
+			$services = @(Get-CimInstance -ClassName Win32_Service | Where-Object {$_.StartMode -match 'auto' -and $_.State -ne 'Running'})
 		}
+		if ($services.Count -gt 0) {
+			$stat = "WARNING" # or "FAIL"
+			$services | Foreach-Object {$tempdata.Add($_.Name)}
+			$msg = "$($services.Count) stopped services were found"
+			$services | Foreach-Object {$tempdata.Add($_.Name)}
+		}
+
 		#>
 	}
 	catch {
