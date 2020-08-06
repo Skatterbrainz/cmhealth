0961705f6f4c1f493c3a016335d1cbc965b8c1a5
Skatterbrainz <dstein2017@gmail.com>
2020-08-05 23:48:22 -0400

    fixes and updates to output data

--- tests/Test-CmCollectionRefresh.ps1
+++ tests/Test-CmCollectionRefresh.ps1
@@ -13,27 +13,33 @@ function Test-CmCollectionRefresh {
 		[parameter()][hashtable] $ScriptParams
 	)
 	try {
+		[int]$maxcolls  = Get-CmHealthDefaultValue -KeySet "configmgr:MaxCollectionRefreshCount" -DataSet $CmHealthConfig
+		Write-Verbose "max collections = $maxcolls"
 		[System.Collections.Generic.List[PSObject]]$tempdata = @() # for detailed test output to return if needed
 		$stat = "PASS"
-		$msg = "No issues found"
-		$query = "Select (Case when RefreshType = 1 then 'Manual'
+		$msg  = "No issues found"
+		$query = "Select 
+(case 
+when RefreshType = 1 then 'Manual'
 when RefreshType = 2 then 'Scheduled'
 when RefreshType = 4 then 'Incremental'
 when RefreshType = 6 then 'Scheduled and Incremental'
-else 'Unknown' end) as RefreshType, count(SiteID) as Collections
-from v_Collections
-group by RefreshType"
+else 'Unknown' end) as RefreshType, 
+SiteID, CollectionName
+from v_Collections"
 		if ($ScriptParams.Credential) {
 			$res = @(Invoke-DbaQuery -SqlInstance $ScriptParams.SqlInstance -Database $ScriptParams.Database -Query $query -SqlCredential $ScriptParams.Credential)
 		} else {
 			$res = @(Invoke-DbaQuery -SqlInstance $ScriptParams.SqlInstance -Database $ScriptParams.Database -Query $query)
 		}
-		$c1 = $res | Where-Object {$_.RefreshType -eq 'Incremental'} | Select-Object -ExpandProperty Collections
-		$c2 = $res | Where-Object {$_.RefreshType -eq 'Scheduled'} | Select-Object -ExpandProperty Collections
-		$c3 = $c1 + $c2
-		if ($c3 -gt 200) {
-			$stat = "FAIL"
-			$msg  = "More than 200 collections are incremental or scheduled"
+		$c1 = ($res | Where-Object {$_.RefreshType -eq 'Incremental'})
+		$c2 = ($res | Where-Object {$_.RefreshType -eq 'Scheduled'})
+		[int]$c3 = $c1.Count + $c2.Count
+		if ($c3 -gt $maxcolls) {
+			$stat = "WARNING"
+			$msg  = "Found $c3 collections are set to incremental or scheduled refresh"
+			$c1 | Foreach-Object {$tempdata.Add( "ID=$($_.SiteID), Name=$($_.CollectionName), RefreshType=$($_.RefreshType)")}
+			$c2 | Foreach-Object {$tempdata.Add( "ID=$($_.SiteID), Name=$($_.CollectionName), RefreshType=$($_.RefreshType)")}
 		}
 	}
 	catch {
