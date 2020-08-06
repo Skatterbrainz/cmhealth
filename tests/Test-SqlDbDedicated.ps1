a95053e81e76486471fda33634670dd9dc043989
skatterbrainz <dstein2017@gmail.com>
2020-08-05 23:14:03 -0400

    added config source

--- tests/Test-SqlDbDedicated.ps1
+++ tests/Test-SqlDbDedicated.ps1
@@ -9,26 +9,29 @@ function Test-SqlDbDedicated {
 	try {
 		$stat = 'PASS'
 		$msg  = "No issues found"
-		$supported = ('master','tempdb','msdb','model','SUSDB','ReportServer','ReportServerTempDB')
+		$supported = @('master','tempdb','msdb','model','SUSDB','ReportServer','ReportServerTempDB')
 		if ($ScriptParams.Credential) {
 			$dbnames = Get-DbaDatabase -SqlInstance $ScriptParams.SqlInstance -SqlCredential $ScriptParams.Credential | Select-Object -ExpandProperty Name
 		} else { 
 			$dbnames = Get-DbaDatabase -SqlInstance $ScriptParams.SqlInstance | Select-Object -ExpandProperty Name
 		}
+		$dblist = @()
 		$dbnames | ForEach-Object {
 			if (-not (($_ -match 'CM_') -or ($_ -in $supported))) {
-				throw "Unsupported database: $($_)"
+				$dblist += $_
 			}
 		}
-		$msg = "All databases are supported for ConfigMgr licensing"
+		if ($dblist.Count -gt 0) {
+			$stat = "FAIL"
+			$msg  = "Databases found which are not supported by MEMCM SQL licensing"
+			$tempdata.Add($dblist)
+		} else {
+			$msg = "All databases are supported for MEMCM SQL licensing"
+		}
 	}
 	catch {
 		$msg = $_.Exception.Message -join ';'
-		if ($msg -match 'Unsupported database') {
-			$stat = "FAIL"
-		} else {
-			$stat = "ERROR"
-		}
+		$stat = "ERROR"
 	}
 	finally {
 		Write-Output $([pscustomobject]@{
