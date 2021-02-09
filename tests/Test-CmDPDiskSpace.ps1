e3c61cd3dd2a5739a949e2a73a80d379b2f533c4
skatterbrainz <dstein2017@gmail.com>
2021-02-09 02:41:00 -0500

    new test file

--- tests/Test-CmDPDiskSpace.ps1
+++ tests/Test-CmDPDiskSpace.ps1
@@ -7,31 +7,39 @@ function Test-CMDPDiskSpace {
 		[parameter()][hashtable] $ScriptParams
 	)
 	try {
-		#[int]$Setting = Get-CmHealthDefaultValue -KeySet "keygroup:keyname" -DataSet $CmHealthConfig
+		[int]$MaxPctUsed = Get-CmHealthDefaultValue -KeySet "siteservers:DiskSpaceMaxPercent" -DataSet $CmHealthConfig
+		Write-Verbose "MaxPctUsed = $MaxPctUsed"
+
 		[System.Collections.Generic.List[PSObject]]$tempdata = @() # for detailed test output to return if needed
 		$stat = "PASS" # do not change this
 		$msg  = "No issues found" # do not change this either
-		if ($ScriptParams.ComputerName -ne $env:COMPUTERNAME) {
-			if ($ScriptParams.Credential) {
-				$cs = New-CimSession -Credential $ScriptParams.Credential -Authentication Negotiate -ComputerName $ScriptParams.ComputerName
+		Write-Verbose "requesting list of DP servers"
+		$query = "select ServerName from v_DistributionPointInfo order by ServerName"
+		[array]$dplist = Invoke-DbaQuery -SqlInstance $SqlInstance -Database $Database -Query $query
+		Write-Verbose "$($dplist.Count) DP server names returned"
+		foreach ($dp in $dplist) {
+			if ($ScriptParams.ComputerName -ne $env:COMPUTERNAME) {
+				$cs = New-CimSession -Credential $ScriptParams.Credential -Authentication Negotiate -ComputerName $dp
 				$res = @(Get-CimInstance -CimSession $cs -ClassName Win32_LogicalDisk -Filter "DriveType = 3")
 			} else {
 				$res = @(Get-CimInstance -CimSession $cs -ClassName Win32_LogicalDisk -Filter "DriveType = 3")
 			}
-		} else {
-			$res = @(Get-CimInstance -CimSession $cs -ClassName Win32_LogicalDisk -Filter "DriveType = 3")
-		}
-		if ($res.Count -gt 0) {
-			foreach ($disk in $res) {
-				$used = $disk.Size - $disk.FreeSpace
-				$pct  = $used / $disk.Size
-				if ($pct -gt 0.9) {
-					$tempData.Add(@{Computer=$($env:COMPUTERNAME);Drive=$($disk.DeviceID);Size=$($disk.Size);Used=$pct})
-					$stat = "WARNING"
-				}
+			if ($res.Count -gt 0) {
+				foreach ($disk in $res) {
+					$size = $disk.Size
+					$free = $disk.FreeSpace
+					$used = $size - $free
+					$pct  = $([math]::Round($used / $size, 1)) * 100
+					if ($pct -gt $MaxPctUsed) {
+						$tempData.Add([pscustomobject]@{Computer=$(dp);Drive=$($disk.DeviceID);Size=$($size);Used=$pct})
+						$stat = "WARNING"
+					} else {
+						$tempData.Add([pscustomobject]@{Computer=$(dp);Drive=$($disk.DeviceID);Size=$($size);Used=$pct})
+					}
+				} # foreach
+				$msg = "$($tempData.Count) issues were found"
 			}
-			$msg = "$($tempData.Count) issues were found"
-		}
+		} # foreach
 	}
 	catch {
 		$stat = 'ERROR'
@@ -43,7 +51,7 @@ function Test-CMDPDiskSpace {
 			TestGroup   = $TestGroup
 			TestData    = $tempdata
 			Description = $Description
-			Status      = $stat 
+			Status      = $stat
 			Message     = $msg
 			Credential  = $(if($ScriptParams.Credential){$($ScriptParams.Credential).UserName} else { $env:USERNAME })
 		})
