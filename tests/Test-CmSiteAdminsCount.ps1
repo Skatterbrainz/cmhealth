3ca01ced0044a2f9f423f6363073d5830dc33785
skatterbrainz <dstein2017@gmail.com>
2021-02-23 16:40:24 -0500

    new test

---
+++ tests/Test-CmSiteAdminsCount.ps1
@@ -0,0 +1,50 @@
+function Test-CmSiteAdminsCount {
+	[CmdletBinding()]
+	param (
+		[parameter()][string] $TestName = "Test-CmSiteAdminsCount",
+		[parameter()][string] $TestGroup = "configuration",
+		[parameter()][string] $Description = "Check if more users are full admins than should be",
+		[parameter()][hashtable] $ScriptParams
+	)
+	try {
+		$startTime = (Get-Date)
+		[int]$defaultSetting = Get-CmHealthDefaultValue -KeySet "configmgr:MaxAdministratorsCount" -DataSet $CmHealthConfig
+		[System.Collections.Generic.List[PSObject]]$tempdata = @() # for detailed test output to return if needed
+		$stat   = "PASS" # do not change this
+		$except = "WARNING" # or "FAIL"
+		$msg    = "No issues found" # do not change this either
+		$query = "select LogonName,DisplayName,IsGroup,AdminSID from v_Admins"
+		[array]$res = Get-CmSqlQueryResult -Query $query -Params $ScriptParams
+		if ($res.Count -gt $defaultSetting) {
+			$stat = $except
+			$msg  = "$($res.Count) items found"
+			$res | Foreach-Object {
+				$tempdata.Add(
+					[pscustomobject]@{
+						LogonName = $_.LogonName
+						DisplayName = $_.Displayname
+						IsGroup = $_.IsGroup
+						SID = $_.AdminSID
+					}
+				)
+			}
+		}
+	}
+	catch {
+		$stat = 'ERROR'
+		$msg = $_.Exception.Message -join ';'
+	}
+	finally {
+		$rt = Get-RunTime -BaseTime $startTime
+		Write-Output $([pscustomobject]@{
+			TestName    = $TestName
+			TestGroup   = $TestGroup
+			TestData    = $tempdata
+			Description = $Description
+			Status      = $stat
+			Message     = $msg
+			RunTime     = $rt
+			Credential  = $(if($ScriptParams.Credential){$($ScriptParams.Credential).UserName} else { $env:USERNAME })
+		})
+	}
+}
