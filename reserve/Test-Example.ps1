09cdda37b25963b88703caf3cc8e3ec679bab2a6
skatterbrainz <dstein2017@gmail.com>
2021-02-25 15:07:59 -0500

    updated template format

--- reserve/Test-Example.ps1
+++ reserve/Test-Example.ps1
@@ -32,7 +32,7 @@ function Test-Example {
 
 		$query = ""
 		$res = Get-CmSqlQueryResult -Query $query -Params $ScriptParams
-		if ($null -ne $res -and $res.Count -gt 0) {
+		if ($res.Count -gt 0) {
 			$stat = $except
 			$msg  = "$($res.Count) items found"
 			#$res | Foreach-Object {$tempdata.Add( [pscustomobject]@{Name=$_.Name} )}
@@ -74,7 +74,6 @@ function Test-Example {
 		$msg = $_.Exception.Message -join ';'
 	}
 	finally {
-		$rt = Get-RunTime -BaseTime $startTime
 		Write-Output $([pscustomobject]@{
 			TestName    = $TestName
 			TestGroup   = $TestGroup
@@ -82,7 +81,7 @@ function Test-Example {
 			Description = $Description
 			Status      = $stat
 			Message     = $msg
-			RunTime     = $rt
+			RunTime     = $(Get-RunTime -BaseTime $startTime)
 			Credential  = $(if($ScriptParams.Credential){$($ScriptParams.Credential).UserName} else { $env:USERNAME })
 		})
 	}
