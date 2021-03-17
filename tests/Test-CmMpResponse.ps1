2c22b292cd17d1f97ad0c8ca90bbf6ba7503efd9
skatterbrainz <dstein2017@gmail.com>
2021-03-17 01:10:50 -0400

    iterate multiple MP systems

--- tests/Test-CmMpResponse.ps1
+++ tests/Test-CmMpResponse.ps1
@@ -6,13 +6,20 @@ function Test-CmMpResponse {
 		[parameter()][string] $Description = "Validate MP web service reponse",
 		[parameter()][hashtable] $ScriptParams
 	)
-	try {
-		$startTime = (Get-Date)
-		[System.Collections.Generic.List[PSObject]]$tempdata = @() # for detailed test output to return if needed
-		$stat   = "PASS"
-		$except = "FAIL"
-		$msg    = "No issues found"
-		$Server = $($env:COMPUTERNAME).ToUpper()
+	$startTime = (Get-Date)
+	[System.Collections.Generic.List[PSObject]]$tempdata = @() # for detailed test output to return if needed
+	$stat   = "PASS"
+	$except = "FAIL"
+	$msg    = "No issues found"
+	$query = "SELECT srs.ServerName,srs.SiteCode,vs.SiteName,vst.AD_Site_Name0 as ADSite,
+vs.ReportingSiteCode as Parent,vs.Installdir
+FROM v_SystemResourceList as srs
+LEFT JOIN v_site vs on srs.ServerName = vs.ServerName
+LEFT JOIN v_R_System_Valid vst on LEFT(srs.ServerName, CHARINDEX('.', srs.ServerName) - 1) = vst.Netbios_Name0
+WHERE srs.RoleName = 'SMS Management Point'
+ORDER BY srs.ServerName"
+	$servers = Get-CmSqlQueryResult -Query $query -Params $ScriptParams
+	foreach ($server in $servers.ServerName) {
 		$URL1 = "http://$Server/sms_mp/.sms_aut?mpcert"
 		$URL2 = "http://$Server/sms_mp/.sms_aut?mplist"
 		$WEBObject1 = [System.Net.WebRequest]::Create($URL1)
@@ -33,6 +40,15 @@ function Test-CmMpResponse {
 			if (($MpcertStatusCode -ne "200") -or ($MplistStatusCode -ne "200")) {
 				$stat = $except
 				$msg = "Invalid web response"
+				$tempdata.Add(
+					[pscustomobject]@{
+						SiteServer = $server
+						MPCertStatus = $MpcertStatusCode
+						MPCertUrl = $URL1
+						MPListStatus = $MplistStatusCode
+						MPListUrl = $URL2
+					}
+				)
 			}
 		}
 		catch {
@@ -42,23 +58,17 @@ function Test-CmMpResponse {
 			$MplisttStatusCode = ( $_.Exception.Response.StatusCode -as [int])
 			$stat = "ERROR"
 			$msg = "Response: MPCERT $($Mpcertstatus) $($MpcertStatusCode) / MPLIST $($MplisttStatus) $($MplisttStatusCode)"
-		}
-	}
-	catch {
-		$stat = 'ERROR'
-		$msg = $_.Exception.Message -join ';'
-	}
-	finally {
-		$rt = Get-RunTime -BaseTime $startTime
-		Write-Output $([pscustomobject]@{
-			TestName    = $TestName
-			TestGroup   = $TestGroup
-			TestData    = $tempdata
-			Description = $Description
-			Status      = $stat
-			Message     = $msg
-			RunTime     = $rt
-			Credential  = $(if($ScriptParams.Credential){$($ScriptParams.Credential).UserName} else { $env:USERNAME })
-		})
-	}
+		}		
+	} # foreach
+
+	Write-Output $([pscustomobject]@{
+		TestName    = $TestName
+		TestGroup   = $TestGroup
+		TestData    = $tempdata
+		Description = $Description
+		Status      = $stat
+		Message     = $msg
+		RunTime     = $(Get-RunTime -BaseTime $startTime)
+		Credential  = $(if($ScriptParams.Credential){$($ScriptParams.Credential).UserName} else { $env:USERNAME })
+	})
 }
