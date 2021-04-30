9474641e67b153e48614c28dc38e32c432f58c07
skatterbrainz <dstein2017@gmail.com>
2021-04-30 15:37:37 -0400

    new tests

---
+++ tests/Test-CmComponentErrors.ps1
@@ -0,0 +1,82 @@
+function Test-CmComponentErrors {
+	[CmdletBinding()]
+	param (
+		[parameter()][string] $TestName = "Site Component Errors",
+		[parameter()][string] $TestGroup = "operation",
+		[parameter()][string] $Description = "Site Component Errors and Warnings",
+		[parameter()][hashtable] $ScriptParams
+	)
+	try {
+		$startTime = (Get-Date)
+		#[int]$Setting = Get-CmHealthDefaultValue -KeySet "keygroup:keyname" -DataSet $CmHealthConfig
+		[System.Collections.Generic.List[PSObject]]$tempdata = @() # for detailed test output to return if needed
+		$stat   = "PASS" # do not change this
+		$except = "WARNING" # or "FAIL"
+		$msg    = "No issues found" # do not change this either
+		$query = "Select 
+ComponentName,
+ComponentType,
+Case
+when Status = 0 then 'OK'
+when Status = 1 then 'Warning'
+when Status = 2 then 'Critical'	
+End as 'Status',
+Case
+when State = 0 then 'Stopped'
+when State = 1 then 'Started'
+when State = 2 then 'Paused'
+when State = 3 then 'Installing'
+when State = 4 then 'Re-installing'
+when State = 5 then 'De-installing'
+End as 'State',
+Case
+When AvailabilityState = 0 then 'Online'
+When AvailabilityState = 3 then 'Offline'
+When AvailabilityState = 4 then 'Unknown'
+End as 'AvailabilityState',
+Infos,
+Warnings,
+Errors
+from vSMS_ComponentSummarizer
+where TallyInterval = N'0001128000100008'
+and MachineName = 'vm-sccmsite-01.uhs.med'
+and SiteCode = 'S02'
+and Status in (1,2)
+Order by Status,ComponentName"
+		$res = Get-CmSqlQueryResult -Query $query -Params $ScriptParams
+		if ($res.Count -gt 0) {
+			$stat = $except
+			$msg  = "$($res.Count) component status issues found"
+			$res | Foreach-Object {
+                $tempdata.Add(
+                    [pscustomobject]@{
+                        ComponentName = $_.ComponentName
+                        ComponentType = $_.ComponentType
+                        Status = $_.Status
+                        State  = $_.State
+                        AvailabilityState = $_.AvailabilityState
+                        Info = $_.Infos
+                        Warnings = $_.Warnings
+                        Errors = $_.Errors
+                    }
+                )
+            }
+		}
+	}
+	catch {
+		$stat = 'ERROR'
+		$msg = $_.Exception.Message -join ';'
+	}
+	finally {
+		Write-Output $([pscustomobject]@{
+			TestName    = $TestName
+			TestGroup   = $TestGroup
+			TestData    = $tempdata
+			Description = $Description
+			Status      = $stat
+			Message     = $msg
+			RunTime     = $(Get-RunTime -BaseTime $startTime)
+			Credential  = $(if($ScriptParams.Credential){$($ScriptParams.Credential).UserName} else { $env:USERNAME })
+		})
+	}
+}
---
+++ tests/Test-SqlDiskSpace.ps1
@@ -0,0 +1,97 @@
+function Test-CMDPDiskSpace {
+	[CmdletBinding()]
+	param (
+		[parameter()][string] $TestName = "SQL Instance Disk Space",
+		[parameter()][string] $TestGroup = "operation",
+		[parameter()][string] $Description = "Check disk space status on SQL Instance",
+		[parameter()][hashtable] $ScriptParams
+	)
+	try {
+		$startTime = (Get-Date)
+		[int]$MaxPctUsed = Get-CmHealthDefaultValue -KeySet "siteservers:DiskSpaceMaxPercent" -DataSet $CmHealthConfig
+		Write-Log -Message "MaxPctUsed = $MaxPctUsed"
+		[System.Collections.Generic.List[PSObject]]$tempdata = @() # for detailed test output to return if needed
+		$stat = "PASS" # do not change this
+		$except = "WARNING"
+		$msg  = "No issues found" # do not change this either
+		$issues = 0
+		Write-Log -Message "requesting list of DP servers"
+		$query = "select ServerName from dbo.v_DistributionPointInfo order by ServerName"
+		[array]$dplist = Invoke-DbaQuery -SqlInstance $SqlInstance -Database $Database -Query $query | Select-Object -ExpandProperty ServerName
+		Write-Log -Message "$($dplist.Count) DP server names returned"
+		[string]$myFQDN=(Get-CimInstance Win32_ComputerSystem).DNSHostName+"."+(Get-CimInstance Win32_ComputerSystem).Domain
+		[int]$index=1
+		foreach ($dp in $dplist) {
+			$res = $null; $cs = $null
+			if ($dp -ne $myFQDN) {
+				Write-Log -Message "connecting to remote DP [$index of $($dplist.Count)]: $dp"
+				$cs = New-CimSession -Credential $ScriptParams.Credential -Authentication Negotiate -ComputerName $dp -ErrorAction SilentlyContinue
+				if ($null -ne $cs) {
+					$res = @(Get-CimInstance -CimSession $cs -ClassName Win32_LogicalDisk -Filter "DriveType = 3" -ErrorAction SilentlyContinue)
+				} else {
+					$res = $null
+				}
+			} else {
+				Write-Log -Message "connecting to local DP [$index of $($dplist.Count)]: $dp"
+				$res = @(Get-CimInstance -ClassName Win32_LogicalDisk -Filter "DriveType = 3")
+			}
+			if ($res.Count -gt 0) {
+				foreach ($disk in $res) {
+					$size = $disk.Size
+					$free = $disk.FreeSpace
+					$used = $size - $free
+					$pct  = $([math]::Round($used / $size, 1)) * 100
+					if ($pct -gt $MaxPctUsed) {
+						$tempData.Add(
+							[pscustomobject]@{
+								Computer = $($dp)
+								Drive    = $($disk.DeviceID)
+								SizeGB   = [math]::Round($size / 1GB, 1)
+								PctUsed  = $pct
+							}
+						)
+						$stat = $except
+						$issues++
+					} else {
+						$tempData.Add(
+							[pscustomobject]@{
+								Computer = $($dp)
+								Drive    = $($disk.DeviceID)
+								SizeGB   = [math]::Round($size / 1GB, 1)
+								PctUsed  = $pct
+							}
+						)
+					}
+				} # foreach
+				if ($issues -gt 0) { $msg = "$issues issues were found" }
+			} else {
+				Write-Warning "DP disk information is not available: $dp"
+				$tempData.Add(
+					[pscustomobject]@{
+						Computer = $($dp)
+						Drive    = $null
+						SizeGB   = $null
+						PctUsed  = $null
+					}
+				)
+			}
+			$index++
+		} # foreach
+	}
+	catch {
+		$stat = 'ERROR'
+		$msg = $_.Exception.Message -join ';'
+	}
+	finally {
+		Write-Output $([pscustomobject]@{
+			TestName    = $TestName
+			TestGroup   = $TestGroup
+			TestData    = $tempdata
+			Description = $Description
+			Status      = $stat
+			Message     = $msg
+			RunTime     = $(Get-RunTime -BaseTime $startTime)
+			Credential  = $(if($ScriptParams.Credential){$($ScriptParams.Credential).UserName} else { $env:USERNAME })
+		})
+	}
+}
