0e147adbe17559356ac318cec670947179db10fd
skatterbrainz <dstein2017@gmail.com>
2021-05-01 01:50:56 -0400

    new test in backlog

--- backlog/Test-CmDiscoveryExclusions.ps1
+++ backlog/Test-CmDiscoveryExclusions.ps1
@@ -15,21 +15,8 @@ function Test-CmDiscoveryExclusions {
 		$msg    = "No issues found" # do not change this either
 		<#
 		=======================================================
-		|	COMMENT: DELETE THIS BLOCK WHEN FINISHED:
-		|
-		|	perform test and return result as an object...
-		|		$stat = $except (no need to set "PASS" since it's the default)
-		|		$msg = "custom message that N issues were found"
-		|		add supporting data to $tempdata array if it helps output
-		|		loop output into $tempdata.Add() array to return as TestData param in output
-		=======================================================
-		#>
-
-		<#
-		=======================================================
 		COMMENT: EXAMPLE FOR SQL QUERY RELATED TESTS... DELETE THIS BLOCK IF NOT USED
 		=======================================================
-
 		$query = ""
 		$res = Get-CmSqlQueryResult -Query $query -Params $ScriptParams
 		if ($null -ne $res -and $res.Count -gt 0) {
@@ -37,35 +24,6 @@ function Test-CmDiscoveryExclusions {
 			$msg  = "$($res.Count) items found"
 			#$res | Foreach-Object {$tempdata.Add( [pscustomobject]@{Name=$_.Name} )}
 		}
-		=======================================================
-		#>
-
-		<#
-		=======================================================
-		COMMENT: EXAMPLE FOR WMI/CIM QUERY RELATED TESTS... DELETE THIS BLOCK IF NOT USED
-		=======================================================
-
-		$disks  = Get-WmiQueryResult -ClassName "Win32_LogicalDisk" -Query "DriveType = 3" -Params $ScriptParams
-		foreach ($disk in $disks) {
-			$drv  = $disk.DeviceID
-			$size = $disk.Size
-			$free = $disk.FreeSpace
-			$used = $size - $free
-			$pct  = $([math]::Round($used / $size, 1)) * 100
-			if ($pct -gt $MaxPctUsed) {
-				$stat = $except
-				$msg  = "One or more disks are low on free space"
-			}
-			$tempdata.Add(
-				[pscustomobject]@{
-					Drive   = $drv
-					Size    = $size
-					Used    = $used
-					PctUsed = $pct
-					MaxPct  = $MaxPctUsed
-				}
-			)
-		} # foreach
 		=======================================================
 		#>
 	}
