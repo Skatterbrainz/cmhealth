f0bc49508c23e80dd5ad168023eefd36f9de8814
skatterbrainz <dstein2017@gmail.com>
2021-10-27 14:05:00 -0400

    added functions

--- private/cmhealthutils.ps1
+++ private/cmhealthutils.ps1
@@ -1,3 +1,5 @@
+# Updated for 0.3.12
+
 function Import-CmHealthSettings {
 	[CmdletBinding()]
 	param (
@@ -286,5 +288,154 @@ function Get-WindowsBuildNumber {
 		'10.0.19044' { '21H2' }
 		'10.0.22000' { '21H1' }
 		Default { 'unknown' }
+	}
+}
+
+<#
+CREDIT to Trevor Jones for this function as part of the script that queries
+site status messages. Refer to https://smsagent.blog/2015/07/22/retrieving-configmgr-status-messages-with-powershell/
+#>
+function Get-StatusMessage {
+	param (
+		$iMessageID,
+		[ValidateSet("srvmsgs.dll","provmsgs.dll","climsgs.dll")]$DLL,
+		[ValidateSet("Informational","Warning","Error")]$Severity,
+		$InsString1,
+		$InsString2,
+		$InsString3,
+		$InsString4,
+		$InsString5,
+		$InsString6,
+		$InsString7,
+		$InsString8,
+		$InsString9,
+		$InsString10
+	)
+	 
+	if ($DLL -eq "srvmsgs.dll")	{ $stringPathToDLL = "$SMSMSGSLocation\srvmsgs.dll" }
+	if ($DLL -eq "provmsgs.dll") { $stringPathToDLL = "$SMSMSGSLocation\provmsgs.dll" }
+	if ($DLL -eq "climsgs.dll") { $stringPathToDLL = "$SMSMSGSLocation\climsgs.dll" }
+	 
+	#Load Status Message Lookup DLL into memory and get pointer to memory
+	$ptrFoo = $Win32LoadLibrary::LoadLibrary($stringPathToDLL.ToString())
+	$ptrModule = $Win32GetModuleHandle::GetModuleHandle($stringPathToDLL.ToString()) 
+	 
+	if ($Severity -eq "Informational") { $code = 1073741824 }
+	if ($Severity -eq "Warning") { $code = 2147483648 }
+	if ($Severity -eq "Error") { $code = 3221225472 }
+	 
+	$result = $Win32FormatMessage::FormatMessage($flags, $ptrModule, $Code -bor $iMessageID, 0, $stringOutput, $sizeOfBuffer, $stringArrayInput)
+	if ($result -gt 0) {
+		# Add insert strings to message
+		$objMessage = New-Object System.Object
+		$objMessage | Add-Member -type NoteProperty -name MessageString -value $stringOutput.ToString().Replace("%11","").Replace("%12","").Replace("%3%4%5%6%7%8%9%10","").Replace("%1",$InsString1).Replace("%2",$InsString2).Replace("%3",$InsString3).Replace("%4",$InsString4).Replace("%5",$InsString5).Replace("%6",$InsString6).Replace("%7",$InsString7).Replace("%8",$InsString8).Replace("%9",$InsString9).Replace("%10",$InsString10)
+	}
+	$objMessage
+}
+
+function Get-SiteStatusMessages {
+	[CmdletBinding()]
+	param ($Params)
+	try {
+		$site =Get-CimInstance -ClassName SMS_Site -ComputerName $Params.ComputerName -Namespace "root/sms/site_$($Params.SiteCode)"
+		if ($null -ne $site.InstallDir) {
+			$SMSMSGSLocation = "$($site.InstallDir)\bin\X64\system32\smsmsgs"
+		} else {
+			throw "unable to get installation path"
+		}
+
+		$Query = "
+select smsgs.RecordID,
+CASE smsgs.Severity
+WHEN -1073741824 THEN 'Error'
+WHEN 1073741824 THEN 'Informational'
+WHEN -2147483648 THEN 'Warning'
+ELSE 'Unknown'
+END As 'SeverityName',
+case smsgs.MessageType
+WHEN 256 THEN 'Milestone'
+WHEN 512 THEN 'Detail'
+WHEN 768 THEN 'Audit'
+WHEN 1024 THEN 'NT Event'
+ELSE 'Unknown'
+END AS 'Type',
+smsgs.MessageID, smsgs.Severity, smsgs.MessageType, smsgs.ModuleName,modNames.MsgDLLName, smsgs.Component,
+smsgs.MachineName, smsgs.Time, smsgs.SiteCode, smwis.InsString1,
+smwis.InsString2, smwis.InsString3, smwis.InsString4, smwis.InsString5,
+smwis.InsString6, smwis.InsString7, smwis.InsString8, smwis.InsString9,
+smwis.InsString10
+from v_StatusMessage smsgs
+join v_StatMsgWithInsStrings smwis on smsgs.RecordID = smwis.RecordID
+join v_StatMsgModuleNames modNames on smsgs.ModuleName = modNames.ModuleName
+where smsgs.MachineName = '$($Params.ComputerName)'
+and DATEDIFF(hour,smsgs.Time,GETDATE()) < '$TimeInHours'
+Order by smsgs.Time DESC
+"
+
+		$table = Invoke-DbaQuery -SqlInstance $Params.SqlInstance -Database $Params.Database -Query $Query
+
+#Start PInvoke Code
+$sigFormatMessage = @'
+[DllImport("kernel32.dll")]
+public static extern uint FormatMessage(uint flags, IntPtr source, uint messageId, uint langId, StringBuilder buffer, uint size, string[] arguments);
+'@ 
+ 
+$sigGetModuleHandle = @'
+[DllImport("kernel32.dll")]
+public static extern IntPtr GetModuleHandle(string lpModuleName);
+'@ 
+ 
+$sigLoadLibrary = @'
+[DllImport("kernel32.dll")]
+public static extern IntPtr LoadLibrary(string lpFileName);
+'@ 
+ 
+		$Win32FormatMessage = Add-Type -MemberDefinition $sigFormatMessage -Name "Win32FormatMessage" -Namespace Win32Functions -PassThru -Using System.Text
+		$Win32GetModuleHandle = Add-Type -MemberDefinition $sigGetModuleHandle -Name "Win32GetModuleHandle" -Namespace Win32Functions -PassThru -Using System.Text
+		$Win32LoadLibrary = Add-Type -MemberDefinition $sigLoadLibrary -Name "Win32LoadLibrary" -Namespace Win32Functions -PassThru -Using System.Text
+		#End PInvoke Code 
+ 
+		$sizeOfBuffer = [int]16384
+		$stringArrayInput = {"%1","%2","%3","%4","%5", "%6", "%7", "%8", "%9"}
+		$flags = 0x00000800 -bor 0x00000200
+		$stringOutput = New-Object System.Text.StringBuilder $sizeOfBuffer 
+ 
+		# Put desired fields into an object for each result
+		$StatusMessages = @()
+
+		foreach ($Row in $Table) {
+			$Params = @{
+				iMessageID  = $Row.MessageID
+				DLL         = $Row.MsgDLLName
+				Severity    = $Row.SeverityName
+				InsString1  = $Row.InsString1
+				InsString2  = $Row.InsString2
+				InsString3  = $Row.InsString3
+				InsString4  = $Row.InsString4
+				InsString5  = $Row.InsString5
+				InsString6  = $Row.InsString6
+				InsString7  = $Row.InsString7
+				InsString8  = $Row.InsString8
+				InsString9  = $Row.InsString9
+				InsString10 = $Row.InsString10
+			}
+			$Message = Get-StatusMessage @params
+ 
+			$StatusMessage = New-Object psobject
+			Add-Member -InputObject $StatusMessage -Name Severity -MemberType NoteProperty -Value $Row.SeverityName
+			Add-Member -InputObject $StatusMessage -Name Type -MemberType NoteProperty -Value $Row.Type
+			Add-Member -InputObject $StatusMessage -Name SiteCode -MemberType NoteProperty -Value $Row.SiteCode
+			Add-Member -InputObject $StatusMessage -Name DateTime -MemberType NoteProperty -Value $Row.Time
+			Add-Member -InputObject $StatusMessage -Name System -MemberType NoteProperty -Value $Row.MachineName
+			Add-Member -InputObject $StatusMessage -Name Component -MemberType NoteProperty -Value $Row.Component
+			Add-Member -InputObject $StatusMessage -Name Module -MemberType NoteProperty -Value $Row.ModuleName
+			Add-Member -InputObject $StatusMessage -Name MessageID -MemberType NoteProperty -Value $Row.MessageID
+			Add-Member -InputObject $StatusMessage -Name Description -MemberType NoteProperty -Value $Message.MessageString
+			$StatusMessages += $StatusMessage
+		}
+		$StatusMessages
+	}
+	catch {
+		Write-Error $_.Exception.Message 
 	}
 }
